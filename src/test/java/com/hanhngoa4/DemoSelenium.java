0afd5d2c910d0b947bbed298efa90ce9253b08ab
hanhngoa4-netizen <hanhngoa4@gmail.com>
2026-05-09 23:06:03 +0700

    Add initial Maven Selenium project
    Create initial project scaffold: add pom.xml (Java 22, selenium-java 4.11.0), .gitignore, and IntelliJ project files (.idea/*). Include a sample Main.java and a DemoSelenium test that launches ChromeDriver, opens a CRM login page and performs basic login actions. Also sets project encoding and basic IDE settings. Note: WebDriver binaries/manager are not configured here and must be provided when running tests.

---
+++ src/test/java/com/hanhngoa4/DemoSelenium.java
@@ -0,0 +1,38 @@
+package com.hanhngoa4;
+
+import org.openqa.selenium.WebDriver;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.edge.EdgeDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
+
+import java.time.Duration;
+
+public class DemoSelenium
+{
+
+    public static void main(String[] args) {
+
+
+            WebDriver driver = new ChromeDriver();
+
+            driver.manage().window().maximize();
+            driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(10));
+            driver.manage().timeouts().pageLoadTimeout(Duration.ofSeconds(30));
+
+            driver.get("https://crm.anhtester.com/admin/authentication");
+
+            driver.findElement(By.xpath("//input[@id='email']")).sendKeys("admin@example.com");
+            driver.findElement(By.xpath("//input[@id='password']")).sendKeys("123456");
+            driver.findElement(By.xpath("//button[normalize-space()='Login']")).click();
+        try {
+            Thread.sleep(2000);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+            driver.quit();
+        }
+
+}
